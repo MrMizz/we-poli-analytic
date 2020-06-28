dbe31aacbe7f035dbfc293abc8062370602ada18
Alexander Binaei <alex.binaei@carpe.io>
2020-06-27 20:15:41 -0700

    dropped aggregators jobs.

--- src/main/scala/in/tap/we/poli/analytic/Main.scala
+++ src/main/scala/in/tap/we/poli/analytic/Main.scala
@@ -4,7 +4,6 @@ import in.tap.base.spark.jobs.composite.CompositeJob
 import in.tap.base.spark.main.InArgs.{OneInArgs, TwoInArgs}
 import in.tap.base.spark.main.OutArgs.OneOutArgs
 import in.tap.base.spark.main.{InArgs, OutArgs}
-import in.tap.we.poli.analytic.jobs.aggregators.{CommitteesAggregateJob, FilerVendorTransactionsJob}
 import in.tap.we.poli.analytic.jobs.connectors.VendorsConnectorJob
 import in.tap.we.poli.analytic.jobs.mergers.VendorsMergerJob
 import in.tap.we.poli.analytic.jobs.transformers.VendorsTransformerJob
@@ -14,10 +13,6 @@ object Main extends in.tap.base.spark.main.Main {
 
   override def job(step: String, inArgs: InArgs, outArgs: OutArgs)(implicit spark: SparkSession): CompositeJob = {
     step match {
-      case "transactions-group-by" =>
-        new FilerVendorTransactionsJob(inArgs.asInstanceOf[OneInArgs], outArgs.asInstanceOf[OneOutArgs])
-      case "cmte-agg" =>
-        new CommitteesAggregateJob(inArgs.asInstanceOf[OneInArgs], outArgs.asInstanceOf[OneOutArgs])
       case "vendors-transformer" =>
         new VendorsTransformerJob(inArgs.asInstanceOf[OneInArgs], outArgs.asInstanceOf[OneOutArgs])
       case "vendors-connector" =>
