ab131fa6153b5651fbb32f30351d32a0a6e5b6fc
Alexander Binaei <alex.binaei@carpe.io>
2020-07-05 18:36:01 -0700

    force remove target, before assembly.

--- publish-jar.sh
+++ publish-jar.sh
@@ -2,5 +2,7 @@
 
 jarDate="latest"
 
+rm -rf target
+
 sbt assembly
 aws s3 cp target/scala-2.11/we-poli-analytic-assembly-1.0.0-SNAPSHOT.jar s3://big-time-tap-in-spark/poli/jars/$jarDate/ --profile tap-in
