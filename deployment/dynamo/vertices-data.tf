d02dc8333305b0a32751f5ca3b9213cafcf15f3a
Alexander Binaei <alex.binaei@carpe.io>
2020-08-15 22:19:28 -0700

    vertices name autocomplete dynamo tf

--- deployment/dynamo/vertices-data.tf
+++ deployment/dynamo/vertices-data.tf
@@ -19,10 +19,10 @@ resource "aws_dynamodb_table" "poli-vertex" {
 
 resource "aws_dynamodb_table" "poli-vertex-name-autocomplete" {
   name = "PoliVertexNameAutoComplete"
-  #billing_mode = "PAY_PER_REQUEST"
-  billing_mode = "PROVISIONED"
-  read_capacity = 5
-  write_capacity = 2500
+  billing_mode = "PAY_PER_REQUEST"
+  #billing_mode = "PROVISIONED"
+  #read_capacity = 5
+  #write_capacity = 25000
   hash_key = "prefix"
 
   attribute {
