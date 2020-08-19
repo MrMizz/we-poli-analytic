95674b1edba919e0571eee51bfd6631612490ac4
Alexander Binaei <alex.binaei@carpe.io>
2020-08-18 17:57:31 -0700

    dynamodb.tf

--- deployment/dynamo/dynamodb.tf
+++ deployment/dynamo/dynamodb.tf
@@ -1,9 +1,9 @@
 resource "aws_dynamodb_table" "poli-vertex-name-autocomplete" {
   name = "PoliVertexNameAutoComplete"
-  #billing_mode = "PAY_PER_REQUEST"
-  billing_mode = "PROVISIONED"
-  read_capacity = 5
-  write_capacity = 25000
+  billing_mode = "PAY_PER_REQUEST"
+  #billing_mode = "PROVISIONED"
+  #read_capacity = 5
+  #write_capacity = 25000
   hash_key = "prefix"
 
   attribute {
