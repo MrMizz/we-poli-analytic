1571f1a8b828c93daa295cc0ab1411d10cb47f01
Alexander Binaei <alex.binaei@carpe.io>
2020-08-20 13:57:54 -0700

    VertexData POST proxy

--- deployment/api-gateway/api.tf
+++ deployment/api-gateway/api.tf
@@ -99,6 +99,61 @@ resource "aws_api_gateway_integration_response" "sub-prefix-integration-response
   }
 }
 
+
+################################################################################
+## VERTEX DATA POST REQUEST ####################################################
+################################################################################
+resource "aws_api_gateway_resource" "vertex" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id = aws_api_gateway_resource.poli.id
+  path_part = "vertex"
+}
+
+resource "aws_api_gateway_method" "vertex-method" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.vertex.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+# https://github.com/tillkuhn/yummy-aws/blob/master/terraform/terraform-apigw.tf
+# great example for ddb tf
+resource "aws_api_gateway_integration" "vertex-integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.vertex.id
+  http_method = aws_api_gateway_method.vertex-method.http_method
+  integration_http_method = "POST"
+  type = "AWS"
+  uri = "arn:aws:apigateway:us-west-2:dynamodb:action/BatchGetItem"
+  credentials = "arn:aws:iam::504084586672:role/PoliGraphDataAccess"
+}
+
+resource "aws_api_gateway_method_response" "vertex-response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.vertex.id
+  http_method = aws_api_gateway_method.vertex-method.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+## Mapping reference: https://docs.aws.amazon.com/de_de/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+## conditional? https://stackoverflow.com/questions/32511087/aws-api-gateway-how-do-i-make-querystring-parameters-optional-in-mapping-templa
+resource "aws_api_gateway_integration_response" "vetex-integration-response" {
+  depends_on = [
+    aws_api_gateway_integration.vertex-integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.vertex.id
+  http_method = aws_api_gateway_method.vertex-method.http_method
+  status_code = aws_api_gateway_method_response.vertex-response.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+
 ################################################################################
 ## DEPLOYMENT ##################################################################
 ################################################################################
