7ab2431c84bcb2fec753cfe51fd198d733f31aca
Alexander Binaei <alex.binaei@carpe.io>
2020-08-30 19:51:32 -0700

    vertex name autocomplete job & writer deploy

--- deployment/emr/submit/dynamo-populators.sh
+++ deployment/emr/submit/dynamo-populators.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CLUSTER="j-3FC7ZR7OB15RL"
-RUN_DATE="2020-08-12-01"
+CLUSTER="j-QAX0PU13OLXK"
+RUN_DATE="2020-08-30-01"
 JAR_PATH="s3://big-time-tap-in-spark/poli/jars/latest/we-poli-analytic-assembly-1.0.0-SNAPSHOT.jar"
 
 #########################################################################
@@ -16,9 +16,9 @@ Args=[\
 --packages,com.audienceproject:spark-dynamodb_2.11:1.0.4,\
 $JAR_PATH,\
 --step,dynamo-vertex-name,\
---in1,s3://big-time-tap-in-spark/poli/graph/vertices/union/$RUN_DATE/,\
+--in1,s3://big-time-tap-in-spark/poli/graph/vertices/union/2020-08-12-01/,\
 --in1-format,json,\
---in2,s3://big-time-tap-in-spark/poli/graph/edges/committee-to-vendor/$RUN_DATE/,\
+--in2,s3://big-time-tap-in-spark/poli/graph/edges/committee-to-vendor/2020-08-12-01/,\
 --in2-format,json,\
 --out1,s3://big-time-tap-in-spark/poli/dynamo/vertex-name-autocomplete/$RUN_DATE/,\
 --out1-format,json\
