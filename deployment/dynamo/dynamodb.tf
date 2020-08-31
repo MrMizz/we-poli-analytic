60e2435fe66e7f04fb4b1373c90941336671edb1
Alexander Binaei <alex.binaei@carpe.io>
2020-08-30 20:57:39 -0700

    only using ddb autocomplete table2

--- deployment/dynamo/dynamodb.tf
+++ deployment/dynamo/dynamodb.tf
@@ -1,28 +1,9 @@
-resource "aws_dynamodb_table" "poli-vertex-name-autocomplete" {
-  name = "PoliVertexNameAutoComplete"
+resource "aws_dynamodb_table" "poli-vertex-name-autocomplete2" {
+  name = "PoliVertexNameAutoComplete2"
   billing_mode = "PAY_PER_REQUEST"
   #billing_mode = "PROVISIONED"
   #read_capacity = 5
   #write_capacity = 25000
-  hash_key = "prefix"
-
-  attribute {
-    name = "prefix"
-    type = "S"
-  }
-
-  tags = {
-    Name = "poli"
-    Environment = "dev"
-  }
-}
-
-resource "aws_dynamodb_table" "poli-vertex-name-autocomplete2" {
-  name = "PoliVertexNameAutoComplete2"
-  #billing_mode = "PAY_PER_REQUEST"
-  billing_mode = "PROVISIONED"
-  read_capacity = 5
-  write_capacity = 25000
   hash_key = "prefix"
 
   attribute {
