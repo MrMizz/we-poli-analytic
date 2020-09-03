0a23245120f5aa04342194080af00655cc4b9dc8
Alexander Binaei <alex.binaei@carpe.io>
2020-09-02 20:02:48 -0700

    scale down edge table.

--- deployment/dynamo/dynamodb.tf
+++ deployment/dynamo/dynamodb.tf
@@ -19,10 +19,10 @@ resource "aws_dynamodb_table" "poli-vertex-name-autocomplete2" {
 
 resource "aws_dynamodb_table" "poli-edge" {
   name = "PoliEdge"
-  #billing_mode = "PAY_PER_REQUEST"
-  billing_mode = "PROVISIONED"
-  read_capacity = 5
-  write_capacity = 25000
+  billing_mode = "PAY_PER_REQUEST"
+  #billing_mode = "PROVISIONED"
+  #read_capacity = 5
+  #write_capacity = 25000
   hash_key = "src_id"
   range_key = "dst_id"
 
