74fdb7fde421f4b7f665317ebeab00065e1b3c35
Alexander Binaei <alex.binaei@carpe.io>
2020-12-25 13:13:56 -0800

    12-2020 pipeline.

--- deployment/emr/submit/id-res.sh
+++ deployment/emr/submit/id-res.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CLUSTER="j-3OXVD229RQ4U7"
-RUN_DATE="2020-09-24-01"
+CLUSTER="j-1HDY58M00W483"
+RUN_DATE="2020-12-23-01"
 JAR_PATH="s3://big-time-tap-in-spark/poli/jars/latest/we-poli-analytic-assembly-1.0.0-SNAPSHOT.jar"
 
 ###################################################
@@ -21,7 +21,7 @@ $JAR_PATH,\
 --in2-format,json,\
 --in3,s3://big-time-tap-in-spark/poli/merged/vendors/$RUN_DATE/,\
 --in3-format,json,\
---out1,s3://big-time-tap-in-spark/poli/connector/fuzzy/features/2020-09-25-01/,\
+--out1,s3://big-time-tap-in-spark/poli/connector/fuzzy/features/$RUN_DATE/,\
 --out1-format,json\
 ]
 
@@ -33,22 +33,22 @@ Args=[\
 --class,in.tap.we.poli.analytic.Main,\
 $JAR_PATH,\
 --step,fuzzy-connector-training,\
---in1,s3://big-time-tap-in-spark/poli/connector/fuzzy/features/2020-09-25-01/,\
+--in1,s3://big-time-tap-in-spark/poli/connector/fuzzy/features/$RUN_DATE/,\
 --in1-format,json,\
---out1,s3://big-time-tap-in-spark/poli/connector/fuzzy/training/2020-09-25-01/,\
+--out1,s3://big-time-tap-in-spark/poli/connector/fuzzy/training/$RUN_DATE/,\
 --out1-format,json\
 ]
 
 aws emr add-steps --cluster-id $CLUSTER --profile tap-in \
---steps Type=spark,Name=FuzzyConnector,\
+--steps Type=spark,Name=FuzzyPredictor,\
 Args=[\
 --deploy-mode,cluster,\
---conf,spark.app.name=FuzzyConnector,\
+--conf,spark.app.name=FuzzyPredictor,\
 --class,in.tap.we.poli.analytic.Main,\
 $JAR_PATH,\
 --step,fuzzy-predictor,\
 --in1,s3://big-time-tap-in-spark/poli/merged/vendors/$RUN_DATE/,\
 --in1-format,json,\
---out1,s3://big-time-tap-in-spark/poli/connector/fuzzy/vendors/2020-09-29-02/,\
+--out1,s3://big-time-tap-in-spark/poli/connector/fuzzy/prediction/$RUN_DATE/,\
 --out1-format,json\
 ]
