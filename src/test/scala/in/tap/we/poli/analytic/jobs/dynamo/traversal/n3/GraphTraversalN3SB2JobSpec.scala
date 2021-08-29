a40d4cb3c37436910cecfb833e4844d42251507c
Alexander Binaei <alex.binaei@carpe.io>
2021-08-29 16:54:17 -0700

    n3 test coverage

---
+++ src/test/scala/in/tap/we/poli/analytic/jobs/dynamo/traversal/n3/GraphTraversalN3SB2JobSpec.scala
@@ -0,0 +1,55 @@
+package in.tap.we.poli.analytic.jobs.dynamo.traversal.n3
+
+import in.tap.base.spark.io.{Formats, In, Out}
+import in.tap.base.spark.main.InArgs.OneInArgs
+import in.tap.base.spark.main.OutArgs.OneOutArgs
+import in.tap.we.poli.analytic.jobs.BaseSparkJobSpec
+import in.tap.we.poli.analytic.jobs.dynamo.traversal.Traversal
+import in.tap.we.poli.analytic.jobs.dynamo.traversal.n2.GraphTraversalN2SB2JobFixtures
+import org.apache.spark.sql.{Dataset, SaveMode}
+
+class GraphTraversalN3SB2JobSpec extends BaseSparkJobSpec with GraphTraversalN2SB2JobFixtures {
+
+  it should "build graph traversal look ups from edges" in {
+    val resourcePath: String = {
+      getClass.getResource("../../../../../../../../../dynamo/").toString
+    }
+    val inPath: String = {
+      s"$resourcePath/graph_traversal/n3/in/"
+    }
+    val outPath: String = {
+      s"$resourcePath/graph_traversal/n3/out/"
+    }
+    import spark.implicits._
+    val _: Unit = {
+      edges
+        .toDS()
+        .write
+        .mode(SaveMode.Overwrite)
+        .parquet(inPath)
+      new GraphTraversalN3SB2Job(
+        OneInArgs(In(inPath, Formats.PARQUET)),
+        OneOutArgs(Out(outPath, Formats.PARQUET))
+      ).execute()
+    }
+    val traversals: Dataset[Traversal] = {
+      spark
+        .read
+        .parquet(outPath)
+        .as[Traversal]
+    }
+    traversals
+      .collect()
+      .toList
+      .sortBy(_.src_ids) shouldBe {
+      List(
+        Traversal(
+          src_ids = "5_6_7",
+          page_num = 1L,
+          dst_ids = Seq(4L)
+        )
+      )
+    }
+  }
+
+}
